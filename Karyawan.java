676d83787a4c754d286ac09d61d791823e882559
yusufshafwan <149211741+yusufshafwan@users.noreply.github.com>
2023-11-30 17:50:48 +0700

    Add files via upload

---
+++ Karyawan.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo222205013latihan51gaji;
+
+/**
+ *
+ * @author ASUS
+ */
+class Karyawan {
+protected String nik;
+    protected String nama;
+    protected String jabatan;
+    protected int golongan;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public int getGolongan() {
+        return golongan;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public void setGolongan(int golongan) {
+        this.golongan = golongan;
+    }    
+}
---
+++ Manager.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo222205013latihan51gaji;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Manager extends Karyawan {
+
+    private int kehadiran;
+    private float tunjanganGolongan;
+    private float tunjanganJabatan;
+    private float tunjanganKehadiran;
+
+    public int getKehadiran() {
+        return kehadiran;
+    }
+
+    public void setKehadiran(int kehadiran) {
+        this.kehadiran = kehadiran;
+    }
+
+    public float tunjanganKehadiran(int hadir) {
+        tunjanganKehadiran = kehadiran * 10000;
+        return tunjanganKehadiran;
+    }
+
+    public float tunjanganJabatan(String jabatan) {
+        if (jabatan.equals("Manager")) {
+            tunjanganJabatan = 2000000;
+        } else if (jabatan.equals("Kabag")) {
+            tunjanganJabatan = 1000000;
+        } else {
+            tunjanganJabatan = 0;
+        }
+        return tunjanganJabatan;
+    }
+
+    public float tunjanganGolongan(int golongan) {
+        if (golongan == 1) {
+            tunjanganGolongan = 500000;
+        } else if (golongan == 2) {
+            tunjanganGolongan = 1000000;
+        } else if (golongan == 3) {
+            tunjanganGolongan = 1500000;
+        }
+        return tunjanganGolongan;
+    }
+
+    public float totalGaji() {
+        return tunjanganJabatan + tunjanganGolongan + tunjanganKehadiran;
+    }
+
+    
+}
